e704c8cae6e672567b63147ed66502806ff000aa
Matej Baco <matejbacocom@gmail.com>
2021-12-14 17:02:14 +0100

    Basic project structure, migration verbose schema

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Work in progress ...
+# Netflix Clone - Project setup
+
+- Setup Appwrite and store it's endpoint in `.env` (`APPWRITE_ENDPOINT`)
+- Register user
+- Create project and store it's ID in `.env` (`APPWRITE_PROJECT_ID`)
+- Create API key and store it in `.env` (`APPWRITE_API_KEY`)
+- Run migrations: `node migrations.js`
+- Run seeds: `node seeds.js`
+
+_Node version used: 16.x_
+_Appwrite version: 0.12.x_
+
+_Migrations is database structure (collections, attribudes, indexes)_
+_Seeds is database content (documents, files)_
---
+++ src/migrations.js
@@ -0,0 +1,26 @@
+// movies
+// Do we even want movies?
+
+// shows
+//     name string
+//     description string
+//     thumbnailImageId string
+//     ageRestriction (7 13 16 18)
+//     tags enum[]
+//     genres enum[]
+//     cast string[]
+//     releaseYear
+
+// showSeasons
+//     showId string
+//     sortIndex number
+//     name string
+//     description string
+//     releaseYear
+
+// showEpisodes
+//     showSeasonId string
+//     sortIndex number
+//     name string
+//     description string
+//     durationMinutes number
