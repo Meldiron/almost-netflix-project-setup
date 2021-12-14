eb763a9093913c503c055ce9cc3b961dc6464772
Matej Baco <matejbacocom@gmail.com>
2021-12-14 17:05:35 +0100

    added movies schema verbose

--- src/migrations.js
+++ src/migrations.js
@@ -1,5 +1,13 @@
 // movies
-// Do we even want movies?
+//     name string
+//     description string
+//     thumbnailImageId string
+//     ageRestriction (7 13 16 18)
+//     tags enum[]
+//     genres enum[]
+//     cast string[]
+//     releaseYear
+//     durationMinutes number
 
 // shows
 //     name string
